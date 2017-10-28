9030c0a1e1b5f6f7942116041dc1524f5d4ecea1
devin t brown <toel3my@gmail.com>
2017-10-28 16:42:49 -0700

    finished script argv[1] is projectID stops all instances in cloud without persistent tag

---
+++ README.md
@@ -0,0 +1,5 @@
+You need to generate and API key for the sendgrid api
+then set make a SENDGRID_API_KEY variable and set it equal to that key
+Install the python sendgrid library 
+the argument for the script problem1.py is the projID
+<python problem1.py projectID>
---
+++ problem1.py
@@ -0,0 +1,63 @@
+import googleapiclient.discovery
+from sys import argv
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+import json
+
+
+
+def stopInstance( longestName, compute, projId, zone ):
+    return compute.instances().stop( project = projId, zone = zone, instance = longestName ).execute()
+    
+    
+
+
+def notify( body ):
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("toel3my@gmail.com")
+    to_email = Email("toel3my@gmail.com")
+    subject = "Notification on stop procedure"
+    content = Content("text/plain", body )
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+def main():    
+    
+    compute = googleapiclient.discovery.build('compute','v1')
+    projId = argv[1]
+    warnings = []
+    errors = []
+    stopped = []
+    #gather all restservers
+    instances = compute.instances().aggregatedList( project = projId ).execute()
+
+    #check if their are any items in instances: sort running set and stopped set
+    for zonepath , result in instances['items'].items():
+        if 'instances' in result:
+            for instance in result['instances']:
+                if instance.get('labels', {} ).get('persistent', 'false') == 'false':
+                    zone = zonepath.split('/')[1]
+                    stopRes = stopInstance( instance['name'] , compute , projId , zone )
+                    warnings +=  [ (instance['name'] , w ) for w in stopRes.get( 'warnings' , [] )]
+                    errors += [ (instance['name'] , e ) for e in stopRes.get( 'error' , {} ).get( 'errors' , [] ) ]
+                    if 'error' not in instance:
+                        stopped.append(instance['name'])
+
+    notify( '''
+Stopped instances:
+{}
+Warnings:
+{}
+Errors:
+{}
+'''.format(*map(json.dumps, [stopped, warnings, errors])))
+    
+    
+    
+if __name__ == "__main__":
+    
+    main() 
